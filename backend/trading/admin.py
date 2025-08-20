c23861e0792e0b5ea73c77a121d8914a31b291cb
Ayush Kaneriya <ayushkaneriya05@gmail.com>
2025-08-20 11:03:40 +0530

    add models

--- backend/trading/admin.py
+++ backend/trading/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Instrument, Watchlist
+from .models import Instrument, Watchlist,TradeHistory,Account,Position,Order
 
 admin.site.register(Instrument)
 admin.site.register(Watchlist)
+admin.site.register(TradeHistory)
+admin.site.register(Account)
+admin.site.register(Position)
+admin.site.register(Order)
